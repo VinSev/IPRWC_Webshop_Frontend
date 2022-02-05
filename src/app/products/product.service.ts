9307c8fc7b7f0570d21e9d42dd0a32fd123e1f8b
VinSe <vincentmcmaps@gmail.com>
2022-02-05 15:54:25 +0100

    Renaming

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Product} from "./Product.model";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
+import {Product} from "./product.model";
 
 @Injectable({
   providedIn: 'root'
