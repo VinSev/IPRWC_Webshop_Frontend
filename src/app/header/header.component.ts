3c998ae8cfa30eb0625da15aa37de4f74d07de94
VinSe <vincentmcmaps@gmail.com>
2022-02-07 18:35:17 +0100

    Header changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthService} from "../authentication/auth.service";
 import {faShoppingCart, IconDefinition} from "@fortawesome/free-solid-svg-icons";
-import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import {faHeart} from "@fortawesome/free-regular-svg-icons";
 import {ShoppingCartService} from "../shopping-cart/shopping-cart.service";
 
 
