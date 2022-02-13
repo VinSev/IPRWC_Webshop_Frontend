c9980b0d45e499bff6b084ff1b963b915b8879a9
VinSe <vincentmcmaps@gmail.com>
2022-02-13 19:34:10 +0100

    Add to Wishlist functionality added

--- src/app/products/product-list/product-item/product-item.component.ts
+++ src/app/products/product-list/product-item/product-item.component.ts
@@ -4,6 +4,7 @@ import {faShoppingCart, IconDefinition} from '@fortawesome/free-solid-svg-icons'
 import { faEye, faHeart } from '@fortawesome/free-regular-svg-icons';
 import {ShoppingCartService} from "../../../shopping-cart/shopping-cart.service";
 import {NotificationService} from "../../../notification/notification.service";
+import {WishlistService} from "../../../wishlist/wishlist.service";
 
 @Component({
   selector: 'app-product-item',
@@ -19,6 +20,7 @@ export class ProductItemComponent {
   public faShoppingCart: IconDefinition = faShoppingCart;
 
   constructor(public shoppingCartService: ShoppingCartService,
+              public wishlistService: WishlistService,
               public notificationService: NotificationService) { }
 
 }
--- src/app/wishlist/wishlist.service.ts
+++ src/app/wishlist/wishlist.service.ts
@@ -19,11 +19,35 @@ export class WishlistService {
   }
 
   public add(product: Product) {
-
+    let index: number = this.findIndex(product);
+    if(this.exists(index)) {
+      this.notificationService.toastrWarning("Product already present in Wishlist");
+      return;
+    }
+    this.notificationService.toastrInfo("Product added to Wishlist");
+    this._products.push(product);
   }
 
   public remove(product: Product) {
+    let index: number = this.findIndex(product);
+    if(this.exists(index)) {
+      this.notificationService.toastrWarning("Product is not present in Wishlist");
+      return;
+    }
+    this.notificationService.toastrInfo("Product removed from Wishlist");
+    this._products.splice(index, 1);
+  }
 
+  public isEmpty(): boolean {
+    return this._products == null || this._products.length == 0;
+  }
+
+  public getTotalAmountOfProducts(): number {
+    return this._products.length;
+  }
+
+  public clear(): void {
+    this._products = [];
   }
 
   private exists(index: number) {
