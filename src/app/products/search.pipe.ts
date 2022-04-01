c380d1129135328e572831d10ae5f7f1bc5d1c4c
VinSe <vincentmcmaps@gmail.com>
2022-04-01 14:04:49 +0200

    search case sensitivity fix

--- src/app/products/search.pipe.ts
+++ src/app/products/search.pipe.ts
@@ -10,9 +10,14 @@ export class SearchPipe implements PipeTransform {
     if(value == null || value.length == 0 || searchText == null) {
       return value;
     }
+
+    searchText = searchText.toUpperCase()
+
     let products: Product[] = [];
     for(let product of value) {
-      if(product.name.includes(searchText) || product.description.includes(searchText)) {
+      let name: string = product.name.toUpperCase();
+      let description: string = product.description.toUpperCase();
+      if(name.includes(searchText) || description.includes(searchText)) {
         products.push(product);
       }
     }
