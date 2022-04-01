84392d359f3e7c06bee9b38b385d0eb90229a869
VinSe <vincentmcmaps@gmail.com>
2022-04-01 18:09:59 +0200

    heroku host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  baseURL: "http://localhost:8080/api/v1"
+  // baseURL: "http://localhost:8080/api/v1"
+  baseURL: "https://iprwc-herk-backend.herokuapp.com/"
 };
 
